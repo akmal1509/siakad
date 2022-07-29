ee67b41fcacee0894f79b88babab9a57d76fe692
Reza <rezasasamandala@gmail.com>
2022-07-29 20:13:25 +0700

    apdet map

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,7 @@
         <div class="bg-blue-500 rounded-lg px-3 py-5 text-white">
             <h1 class="text-lg font-semibold">Kontak Kami</h1>
             <div class="mt-4 flex flex-col gap-4 text-center">
-                <iframe
-                    src="https://www.google.com/maps/place/SMPN+1+SINDANGKERTA/@-6.9919222,107.4032188,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68f10ef0f04c5d:0x54fa2a5ec1b15fbc!8m2!3d-6.9919222!4d107.4054075"
-                    width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy"
-                    class="rounded-lg col-span-2 w-full" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe  class="rounded-lg col-span-2 w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.1473319634133!2d107.40321881469!3d-6.991922194949087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68f10ef0f04c5d%3A0x54fa2a5ec1b15fbc!2sSMPN%201%20SINDANGKERTA!5e0!3m2!1sen!2sid!4v1659100334353!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <p class="col-span-1 text-sm">
                     <span class="font-semibold text-base">Address</span> : SMPN 1 Sindangkerta, Jalan Raya Sindangkerta, kecamatan sidangkerta, Kabupaten Bandung Barat, Jawa Barat 40563
                 </p>
